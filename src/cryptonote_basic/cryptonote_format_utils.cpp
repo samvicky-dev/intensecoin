8029411c1975ff6aa1d7734df501a5c81c2267b9
valiant1x <valiant1x@users.noreply.github.com>
2018-01-16 10:47:00 -0500

    fix get_block_hashing_blob linking issue

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -718,6 +718,15 @@ namespace cryptonote
     return get_transaction_hash(t, res, &blob_size);
   }
   //---------------------------------------------------------------
+  blobdata get_block_hashing_blob(const block& b)
+  {
+	  blobdata blob = t_serializable_object_to_blob(static_cast<block_header>(b));
+	  crypto::hash tree_root_hash = get_tx_tree_hash(b);
+	  blob.append(reinterpret_cast<const char*>(&tree_root_hash), sizeof(tree_root_hash));
+	  blob.append(tools::get_varint_data(b.tx_hashes.size() + 1));
+	  return blob;
+  }
+  //---------------------------------------------------------------
   bool get_block_hashing_blob(const block& b, blobdata& blob)
   {
     blob = t_serializable_object_to_blob(static_cast<block_header>(b));
