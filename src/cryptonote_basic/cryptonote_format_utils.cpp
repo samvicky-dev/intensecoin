58fc79bfcc12a932778de4938a3864adfc65b5c2
valiant1x <valiant1x@users.noreply.github.com>
2018-01-28 02:27:25 -0500

    fix unused variable warning

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -743,7 +743,7 @@ namespace cryptonote
   //---------------------------------------------------------------
   bool calculate_block_hash(const block& b, crypto::hash& res)
   {
-    crypto::hash block_blob_hash = get_blob_hash(block_to_blob(b));
+    get_blob_hash(block_to_blob(b));
     bool hash_result = get_object_hash(get_block_hashing_blob(b), res);
     return hash_result;
   }
