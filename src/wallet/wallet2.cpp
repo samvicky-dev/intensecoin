e3c2751e877652eee1b380d55ed2ff4e5af69445
valiant1x <valiant1x@users.noreply.github.com>
2018-02-14 12:07:02 -0500

    remove hard fork 2 TX unlock time calculations in XMR, they targeted a different difficulty in HF 2, requiring TX unlock times to be different. ITNS has no such requirements

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -2853,10 +2853,7 @@ bool wallet2::is_tx_spendtime_unlocked(uint64_t unlock_time, uint64_t block_heig
   {
     //interpret as time
     uint64_t current_time = static_cast<uint64_t>(time(NULL));
-    // XXX: this needs to be fast, so we'd need to get the starting heights
-    // from the daemon to be correct once voting kicks in
-    uint64_t v2height = m_testnet ? 624634 : 1009827;
-    uint64_t leeway = block_height < v2height ? CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V1 : CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V2;
+    uint64_t leeway = CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS_V1;
     if(current_time + leeway >= unlock_time)
       return true;
     else
